4617d1e15b7b476cf451b10623a0f87348325ae0
james <rymerjames@yahoo.com>
2024-04-02 12:43:29 -0700

    battlestar

--- src/containers/leaderboard/components/PlayerBar.jsx
+++ src/containers/leaderboard/components/PlayerBar.jsx
@@ -18,7 +18,7 @@ const PlayerBar = (props) => {
                 (   <div className='playerBar closed'>
                         <div className='top'>
                             <div className='left'> 
-                                <img className='battleStar' src="./src/assets/battle-star.png" alt="" />
+                                <img className='battleStar' src="images/battle-star.png" alt="" />
                                 <span className='level'> {props.playerData.battlePass.level}</span>
                                 <h3 className='playerName'>{props.playerData.account.name}</h3>
                             </div>
